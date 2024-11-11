8bcb8d22d4b3a054d8a4d45e9c470af17d6dd897
Maya Touzari <128609408+Maya-Touzari@users.noreply.github.com>
2024-11-11 22:43:23 +0100

    fix: main

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ from src.ml import process_data, inference
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 if "RENDER" in os.environ and os.path.isdir(".dvc"):
-    logging.info("setting up dvc")
+    logging.info("setting up dvc 1")
     if os.path.exists("/var/tmp/dvc"):
         logging.info("remove tmp dir")
         os.system("rm -rf /var/tmp/dvc")
     os.system("dvc config core.no_scm true")
+    logging.info("setting up dvc 2")
     os.system("dvc pull")
 
 # if "RENDER" in os.environ and os.path.exists("/var/tmp/dvc"):
